51057f1404e6a9e8ad6a07510372626ec6b0e7a6
CageStooge <CageStooge@users.noreply.github.com>
2021-08-29 14:54:03 -0400

    Create utils.lua

---
+++ utils.lua
@@ -0,0 +1,47 @@
+function split(inputstr, sep)
+   if sep == nil then
+      sep = '%s'
+   end
+   local t = {}
+   for str in string.gmatch(inputstr, '([^' .. sep .. ']+)') do
+      table.insert(t, str)
+   end
+   return t
+end
+
+--[[ Because there is no wait function in lua this is as good as it will get, but 
+     it's not 100% accurate, a few milliseconds here or there]]
+function wait(msToWait)
+   local millis = computer.millis()
+   while computer.millis() - millis < msToWait do
+      computer.skip()
+   end
+end
+
+--[[ function round: This will round numbers for you to whatever precision level you want,
+     from whole numbeers down to thousands or even more.
+     to get this to a specific decimal place call it with the precison you want writen as 0.x
+     example 1 : round(<some number>,0.1) will round to the nearest tenth
+     example 2 : round( <some number> , 0.01) will return the number rounded to the nearest hundreth and so on
+     example 3 : if precision is not specified it defaults to 1 (whole number rounding) ]]
+function round(number, decimalPlace)
+   if decimalPlace == nil then
+      decimalPlace = 1
+   end
+
+   -- Now we divide the value by the decimalPlace ... this works because math ...
+   local preRound = number / decimalPlace
+
+   -- I stole this from google somewhere --
+   local value = preRound % 1 >= 0.5 and math.ceil(preRound) or math.floor(preRound)
+
+   local desiredPrecision = value * decimalPlace
+   return desiredPrecision
+end
+
+--[[ perMinute: This will take the amount a machine uses or produces and then 
+     multiply it by the result of diving 60 by the cycleTime of the factory ]]
+function perMinute(amount, cycleTime)
+   local perMinute = (60 / cycleTime) * amount
+   return perMinute
+end
